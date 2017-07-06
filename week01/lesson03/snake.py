262a0f22bc92398f8b106318c56a3d3bfcf1bcaa
Luis Fernando Saquetim Vendrame <luis.vendrame@compline.com.br>
2017-07-06 09:46:22 -0300

    First Lessons

---
+++ week01/lesson03/snake.py
@@ -0,0 +1,16 @@
+how_many_snakes = 1
+snake_string = """
+Bem-vindo ao Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3, Philip e Charlie
+"""
+
+
+print(snake_string * how_many_snakes)
